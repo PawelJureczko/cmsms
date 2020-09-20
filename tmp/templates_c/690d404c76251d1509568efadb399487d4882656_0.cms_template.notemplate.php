2cdbc9018e2567e7264124d8cd6a446d7b306efd
Pawel Jureczko <jureczko.pawel@gmail.com>
2020-09-20 20:48:33 +0200

    initial commit

---
+++ tmp/templates_c/690d404c76251d1509568efadb399487d4882656_0.cms_template.notemplate.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.31, created on 2020-09-20 20:41:14
+  from "cms_template:notemplate" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_5f67a24af1f969_50093259',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '690d404c76251d1509568efadb399487d4882656' => 
+    array (
+      0 => 'cms_template:notemplate',
+      1 => 1600627274,
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5f67a24af1f969_50093259 (Smarty_Internal_Template $_smarty_tpl) {
+CMS_Content_Block::smarty_internal_fetch_contentblock(array(),$_smarty_tpl);
+}
+}
